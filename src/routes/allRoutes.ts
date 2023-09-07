f74b117041ee1d399bda6b6953a266edeb9a0471
group5fpt <phulocninh321@gmail.com>
2023-09-08 00:09:29 +0700

    connect db postgres

--- src/routes/allRoutes.ts
+++ src/routes/allRoutes.ts
@@ -1,6 +1,28 @@
 import express from "express"
+import {Pool, Client} from 'pg';
 
 const route = express.Router();
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  password: '123',
+  database: 'dbPhu',
+  port: 5432
+})
+
+pool.query('SELECT * FROM student', (err, res) => {
+  console.log('result is: ',res.rows[0]);
+})
+
+async function insertDBStudent(val:any) {
+  const query = {
+    text: 'INSERT INTO STUDENT(id, name_student, age) VALUES($1, $2, $3)',
+    values: [val.id, val.name, val.age]
+  }
+  await pool.query(query);
+  
+  await pool.end();
+} 
 
 route.get("/", (req, res, next) => {
   const object = { name: "Nguyen Quoc Phu", age: 22, job: "Software Engineer" };
@@ -9,6 +31,7 @@ route.get("/", (req, res, next) => {
 
 route.post("/", (req, res, next) => {
   const body = req.body;
+  insertDBStudent(body);
   console.log(body);
 });
 
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import {Pool, Client} from 'pg';
 import {route} from './routes/allRoutes';
 
 const app = express();
 const port = 3000;
 const hostname = '0.0.0.0'
+
+
 
 app.use(express.json())
 app.use(route);
